e6c7221f4259ea9a339206cd19acbe15a1811762
rahulpahuja <10242693+rahulpahuja@users.noreply.github.com>
2026-05-29 22:06:50 +0530

    Bump Up Version Code

--- DeDup/app/build.gradle.kts
+++ DeDup/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.rp.dedup"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
